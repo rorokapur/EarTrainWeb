c9306a47e743067a0cabbb10860e194a27f7baf0
rorokapur <kapurkrohan@gmail.com>
2024-03-11 15:14:48 -0700

    note selector

--- script.js
+++ script.js
@@ -1,54 +1,93 @@
 let score;
 document.addEventListener('DOMContentLoaded', ()=>{
-    score = new Score(document.getElementById("correctVal"),document.getElementById("incorrectVal"),document.getElementById("meter-green"),document.getElementById("meter-red"));
+    document.querySelectorAll(".note-button").forEach((button)=>{
+        button.addEventListener("click", ()=>{
+            document.getElementById("clear-notes").style.visibility = "visible";
+            let el = document.createElement('div');
+            el.innerText = button.innerText;
+            el.classList.add("note");
+            el.setAttribute("data-note", button.getAttribute("data-note"));
+            document.getElementById("notes").append(el);
+            document.getElementById("notes").scrollBy(100,0);
+            
+        })
+    });
+    document.getElementById("clear-notes").addEventListener("click", ()=>{
+        document.getElementById("notes").innerHTML="";
+        document.getElementById("clear-notes").style.visibility = "hidden";
+
+
+    });
+    score = new ScoreBar(document.getElementById("correctVal"),document.getElementById("incorrectVal"),document.getElementById("meter-green"),document.getElementById("meter-red"));
 })
 
 
 
 
+class ScoreBar{
+    leftText;
+    rightText;
+    leftBar;
+    rightBar;
+    left;
+    right;
 
-class Score{
-    correctText;
-    incorectText;
-    greenEl;
-    redEl;
-    correctVal;
-    incorrectVal;
-
-    constructor(correctText, incorrectText, greenEl, redEl){
-        this.correctText = correctText;
-        this.incorrectText = incorrectText;
-        this.greenEl = greenEl;
-        this.redEl = redEl;
-        this.correctVal = 0;
-        this.incorrectVal = 0;
+    constructor(leftText, rightText, leftBar, rightBar){
+        this.leftText = leftText;
+        this.rightText = rightText;
+        this.leftBar = leftBar;
+        this.rightBar = rightBar;
+        this.left = 0;
+        this.right = 0;
     }
-    correct(){
-        this.correctVal++;
-        this.correctText.innerText = this.correctVal;
-        if(this.incorrectVal == 0){
-            this.greenEl.style.width = "100%";
+    increaseLeft(){
+        this.left++;
+        this.leftText.innerText = this.left;
+        if(this.right == 0){
+            this.leftBar.style.width = "100%";
+            this.rightBar.style.width = "0%"
             return;
         }
-        this.greenEl.style.width = (this.correctVal/(this.incorrectVal + this.correctVal)) * 100 + "%";
-        this.redEl.style.width = (100 - (this.correctVal/(this.incorrectVal + this.correctVal)) * 100) + "%";
+        this.leftBar.style.width = (this.left/(this.right + this.left)) * 100 + "%";
+        this.rightBar.style.width = (100 - (this.left/(this.right + this.left)) * 100) + "%";
     }
-    incorrect(){
-        this.incorrectVal++;
-        this.incorrectText.innerText = this.incorrectVal;
-        if(this.correctVal == 0){
-            this.redEl.style.width = "100%";
+    increaseRight(){
+        this.right++;
+        this.rightText.innerText = this.right;
+        if(this.left == 0){
+            this.rightBar.style.width = "100%";
+            this.leftBar.style.width = "0%"
             return;
         }
-        this.greenEl.style.width = (this.correctVal/(this.incorrectVal + this.correctVal)) * 100 + "%";
-        this.redEl.style.width = (100 - (this.correctVal/(this.incorrectVal + this.correctVal)) * 100) + "%";
+        this.leftBar.style.width = (this.left/(this.right + this.left)) * 100 + "%";
+        this.rightBar.style.width = (100 - (this.left/(this.right + this.left)) * 100) + "%";
+    }
+    decreaseLeft(){
+        if (this.left < 1){return;}
+        this.left--;
+        this.leftText.innerText = this.left;
+        if(this.left < 1){
+            this.leftBar.style.width = "0%";
+        }
+        this.leftBar.style.width = (this.left/(this.right + this.left)) * 100 + "%";
+        this.rightBar.style.width = (100 - (this.left/(this.right + this.left)) * 100) + "%";
+    }
+    decreaseRight(){
+        if (this.right < 1){return;}
+        this.right--;
+        this.rightText.innerText = this.right;
+        if(this.right < 1){
+            this.rightBar.style.width = "0%"
+        }
+        this.leftBar.style.width = (this.left/(this.right + this.left)) * 100 + "%";
+        this.rightBar.style.width = (100 - (this.left/(this.right + this.left)) * 100) + "%";
     }
     reset(){
-        this.correctText.innerText = 0;
-        this.incorrectText.innerText = 0;
-        this.redEl.style.width= "0%";
-        this.greenEl.style.width = "0%";
-        this.correctVal = 0;
-        this.incorrectVal = 0;
+        this.leftText.innerText = 0;
+        this.rightText.innerText = 0;
+        this.rightBar.style.width= "0%";
+        this.leftBar.style.width = "0%";
+        this.left = 0;
+        this.right = 0;
     }
 }
