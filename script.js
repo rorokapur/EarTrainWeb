57220369b1276ffbc884b961ed648ad6be9f0759
rorokapur <kapurkrohan@gmail.com>
2024-05-07 13:04:24 -0700

    bulk changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let score;
 document.addEventListener('DOMContentLoaded', ()=>{
     Controls.init();
-    score = new ScoreBar(document.getElementById("correctVal"),document.getElementById("incorrectVal"),document.getElementById("meter-green"),document.getElementById("meter-red"));
+    et = new EarTraining();
 })
 class DarkMode{
     static dark(){
@@ -81,42 +81,91 @@ class EarTraining{
     controls;
     correct;
     incorrect;
-    listens;
-    constructor(controls, input, score){
-        this.input = input;
-        this.input.querySelector('#enter').addEventListener('click',this.checkAnswer());
-        this.controls = controls;
-        this.controls.querySelector('#play-chord').addEventListener('click', this.playChord());
-        this.controls.querySelector('#play-key').addEventListener('click', this.playKey());
-        this.score = score;
+    keylistens;
+    notelistens;
+    constructor(){
+        this.newChord();
+        this.score = new ScoreBar(document.getElementById("correctVal"),document.getElementById("incorrectVal"),document.getElementById("meter-green"),document.getElementById("meter-red"));
+        this.input = document.getElementById('note-selector');
+        document.getElementById('enter').addEventListener('click',()=>{this.checkAnswer()});
+        this.controls = document.querySelector('#flow-controls');
+        document.getElementById('play-chord').addEventListener('click', ()=>{this.playChord()});
+        document.getElementById('play-key').addEventListener('click', ()=>{this.playKey()});
+    }
+    playKey(){
+        this.keylistens++
+        document.querySelector('#infobox').classList.remove("correct");
+        document.querySelector('#infobox').classList.remove("incorrect");
+        document.querySelector('#bigtext').innerText = "Playing key...";
+        let kp = "s";
+        if (this.keylistens == 1){
+            kp = ""
+        }
+        let np = "s";
+        if (this.notelistens == 1){
+            np = ""
+        }
+        document.querySelector('#littletext').innerText = "Once you know where Do is, listen to the notes.\nYou have listened to the key " + this.keylistens + " time" + kp + " and the notes " + this.notelistens + " time" + np + ".";
+        this.exercise.playKey();
+    }
+    playChord(){
+        this.notelistens++;
+        document.querySelector('#infobox').classList.remove("correct");
+        document.querySelector('#infobox').classList.remove("incorrect");
+        document.querySelector('#bigtext').innerText = "Playing notes...";
+        let kp= "s";
+        if (this.keylistens == 1){
+            kp = ""
+        }
+        let np = "s";
+        if (this.notelistens == 1){
+            np = ""
+        }
+        document.querySelector('#littletext').innerText = "Figure out the solfege of each note and enter your answer below.\nYou have listened to the key " + this.keylistens + " time" + kp + " and the notes " + this.notelistens + " time" + np + ".";
+        this.exercise.playChord();
+    }
+    checkAnswer(){
+        if(this.notelistens == 0){return;}
+        let answer = [];
+        document.querySelectorAll('.note').forEach((el)=>{
+            answer.push(el.dataset.note);
+        })
+        let delay = 0;
+        Array.from(document.querySelectorAll('.note')).reverse().forEach((note)=>{
+
+            setTimeout(()=>{
+                note.style.scale = "0";
+                setTimeout(()=>{note.remove();}, 200);
+            }, delay);
+            delay += 15;
+
+        });
+        if(this.exercise.checkAnswer(answer)){
+            this.score.increaseLeft();
+            this.correct++;
+            this.newChord();
+            document.querySelector('#infobox').classList.remove("incorrect");
+            document.querySelector('#infobox').classList.add("correct");
+            document.querySelector('#bigtext').innerText = "Correct!";
+            document.querySelector('#littletext').innerText = "Play the new key or notes to continue";
+            this.newChord();
+        } else {
+            this.score.increaseRight();
+            this.incorrect++;
+            document.querySelector('#infobox').classList.remove("incorrect");
+            document.querySelector('#infobox').classList.add("incorrect");
+            document.querySelector('#bigtext').innerText = "Incorrect!";
+            document.querySelector('#littletext').innerText = "Give it another listen any try again";
+        }
+    }
+    newChord(){
+        this.keylistens = 0;
+        this.notelistens = 0;
         if (this.randomKey){
             this.exercise = new Exercise(this.notes, this.chromatics);
         } else {
             this.exercise = new Exercise(this.notes, this.chromatics, this.key);
         }
-    }
-    playKey(){
-        this.exercise.playKey();
-    }
-    playChord(){
-        this.exercise.playChord();
-        this.listens++;
-    }
-    checkAnswer(){
-        let answer = [];
-        document.querySelectorAll('.note').forEach((el)=>{
-            answer.push(el.dataset.note);
-        })
-        if(this.exercise.checkAnswer(answer)){
-            this.score.increaseLeft();
-            this.correct++;
-        } else {
-            this.score.increaseRight();
-            this.incorrect++;
-        }
-    }
-    newChord(){
-
     }
 }
 
